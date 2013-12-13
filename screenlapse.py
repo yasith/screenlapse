682039c16f9dfd505c32ff96bb75bf3b3e67e5ae
Yasith Vidanaarachchi <yasith@tuxv.net>
2013-12-12 21:04:32 -0500

    Refactoring and Command Line Arguments
    Changed the code so it’s divided into more functions.
    
    Added command line arguments for time interval and output directory.

--- screenlapse.py
+++ screenlapse.py
@@ -1,30 +1,73 @@
-import subprocess
-import time
+#! /usr/bin/env python3
+import getopt
 import os
+import subprocess
+import sys
+import time
+
+HELP_STR = "screenlapse.py -o <outputdir> -t <time_interval>"
+PREFIX = "screen"
 
 def main():
-	i = 0
-	prefix = "screen"
-	fdir = "~/screencaps"
+
+	interval, fdir = get_args()
+	print("Screenlapse interval:", interval)
+	print("Screenlapse directory:", fdir)
 
 	if not os.path.exists(os.path.expanduser(fdir)):
 		os.mkdir(os.path.expanduser(fdir))
 
-	while 1:
-		fname = prefix + str(i).zfill(10) + ".png"
-		fpath = os.path.expanduser(os.path.join(fdir, fname))
-		if not os.path.exists(fpath):
-			break
-		i += 1
+	frame = find_next(fdir)
 
 	while 1:
-		fname = prefix + str(i).zfill(10) + ".png"
+		take_snap(fdir, interval, frame)
+		frame += 1
+
+def find_next(fdir):
+	frame = 0
+
+	fname = PREFIX + str(frame).zfill(10) + ".png"
+	fpath = os.path.expanduser(os.path.join(fdir, fname))
+
+	while os.path.exists(fpath):
+		frame += 1
+		fname = PREFIX + str(frame).zfill(10) + ".png"
 		fpath = os.path.expanduser(os.path.join(fdir, fname))
-		print(fpath)
-		subprocess.call(["screencapture", "-t", "png", "-x", fpath])
-		time.sleep(1)
-		print(fname)
-		i += 1
+
+	return frame
+
+def get_args():
+	interval = 1
+	fdir = "~/screencaps"       
+
+	argv = sys.argv[1:]
+	try:
+		opts, args = getopt.getopt(argv,"ho:t:", ["out=","time=","help"])
+	except getopt.GetoptError:
+		print(HELP_STR)
+		sys.exit(2)
+
+	for opt, arg in opts:
+		if opt in ("-h", "--help"):
+			print(HELP_STR)
+			sys.exit(0)
+		elif opt in ("-o", "--out"):
+			fdir = arg
+		elif opt in ("-t", "--time"):
+			arg.strip()
+			interval = float(arg)
+
+	return interval, fdir
+
+def take_snap(fdir, interval, frame):
+
+	fname = PREFIX + str(frame).zfill(10) + ".png"
+	fpath = os.path.expanduser(os.path.join(fdir, fname))
+
+	subprocess.call(["screencapture", "-t", "png", "-x", fpath])
+	print(fpath)
+
+	time.sleep(interval)
 
 
 if __name__ == "__main__":
